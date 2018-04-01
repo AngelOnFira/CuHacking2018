1b2d2b81f1831eac6344703d5cc3d80d837cb2e5
Liz Davies <sarah.e.e.davies@gmail.com>
2018-04-01 01:24:56 -0400

    Initial Commit

---
+++ functions/commands/bullyme.js
@@ -0,0 +1,31 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /hello
+*
+*   Basic "Hello World" command.
+*   All Commands use this template, simply create additional files with
+*   different names to add commands.
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+
+
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  var insultBank = ["smells like poo","couldn't find his dick even if he tried","'s mother was a hamster"];
+  let tempInsult = insultBank[Math.floor(Math.random()*insultBank.length)];
+  callback(null, {
+    text: `<@${user}> ${tempInsult}`,
+    attachments: [
+      // You can customize your messages with attachments.
+      // See https://api.slack.com/docs/message-attachments for more info.
+    ]
+  });
+};
